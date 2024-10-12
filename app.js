11f1c2d8ab518a07e3df4062d006722ab5daf03f
Muhammed Abdelghafar <muhammed.abdelghafar@outlook.com>
2024-10-12 15:19:48 +0300

    remove dotenv module

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const app = express();
 const cors = require('cors');
-require('dotenv').config();
 
 
 app.use(bodyParser.json());
